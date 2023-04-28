c76592dec755a4ee3d43000a1c09306e2db2a207
Diego Gava <diego.gava15@gmail.com>
2023-04-27 21:26:42 -0300

    ajustando

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-let palavraSorteada = 'VIVER'
-palavrasPossiveis[Math.floor(Math.random() * palavrasPossiveis.length)].toUpperCase()
+let palavraSorteada = palavrasPossiveis[Math.floor(Math.random() * palavrasPossiveis.length)].toUpperCase()
 const dicionario = listaDePalavras.map(palavra => palavra.toUpperCase());
 let palpites = [[]]
 let contadorPalpite = 0
